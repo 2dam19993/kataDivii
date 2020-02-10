e642c1dcabd2d77d3e9af6a934ba30ae1ffb4bec
2dam19993 <55793601+2dam19993@users.noreply.github.com>
2020-02-10 19:47:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def elSegundo(s,lista):
   frase=list(s)
   nuevaFrase=list(s)
-  cantidad=len(lista)/2
+  cantidad=len(lista)
   indice=0
-  while(indice<=cantidad):
-    nuevaFrase[2*indice]=frase[2*indice+1]
+  while(indice<cantidad):
+    print(indice)
+    print(cantidad)
+    print(len(frase))
+    print(nuevaFrase[lista[indice]])
+    print(frase[lista[cantidad-indice-1]])
+    nuevaFrase[lista[indice]]=frase[lista[cantidad-indice-1]]
     indice=indice+1
   print(nuevaFrase)
 
 listas=[4,0,5,8,11,4]
 print("zasca")
 elSegundo('tienes dos horas',listas)
-
